4a693d01c9ac138c61187d677999de4a24684132
Adam Lynch <contact@adamlynch.com>
2022-03-12 14:18:29 +0000

    If geolocation resolves to Dublin, use Cork instead

--- functions/unknown.js
+++ functions/unknown.js
@@ -2,8 +2,20 @@ export async function onRequestGet({ request }) {
   const parsedUrl = new URL(request.url);
   parsedUrl.pathname = "/geo-lookup-done";
   if (request.cf.latitude && request.cf.longitude) {
-    parsedUrl.pathname +=
-      "/" + [request.cf.latitude, request.cf.longitude].join(",");
+    /*
+      It incorrectly resolves to Dublin sporadically.
+      So I'd rather it just always go to Cork instead of Dublin. Corcaigh Abú!
+    */
+    if (
+      request.cf.city?.toLowerCase() === "dublin" ||
+      (request.cf.latitude.toString().startsWith("53.3") &&
+        request.cf.longitude.toString().startsWith("-6.2"))
+    ) {
+      parsedUrl.pathname += "51.89863,-8.47039";
+    } else {
+      parsedUrl.pathname +=
+        "/" + [request.cf.latitude, request.cf.longitude].join(",");
+    }
   }
   console.log(`Redirecting / to ${parsedUrl}...`);
   return Response.redirect(parsedUrl.toString(), 302);

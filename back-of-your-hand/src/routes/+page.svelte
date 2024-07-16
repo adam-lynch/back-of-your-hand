775514ae099bbe84546a7c5a51df4a5abd654547
Adam <team@readwise.io>
2024-07-16 22:43:40 +0100

    WIP

--- back-of-your-hand/src/routes/+page.svelte
+++ back-of-your-hand/src/routes/+page.svelte
@@ -1,3 +1,9 @@
+<script lang="ts">
+  import type { A } from '$lib/types';
+  const message: A = '!!!2';
+  console.log(message)
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <a href="./example">Example</a>

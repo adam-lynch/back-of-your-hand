6628cc66ec5ea7a1d60911287f10cff88ebcfecd
Adam <team@readwise.io>
2024-07-17 00:17:46 +0100

    WIP

--- back-of-your-hand/vite.config.ts
+++ back-of-your-hand/vite.config.ts
@@ -1,7 +1,11 @@
 import { sveltekit } from "@sveltejs/kit/vite";
+import browserslistToEsbuild from "browserslist-to-esbuild";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  build: {
+    target: browserslistToEsbuild(),
+  },
   plugins: [sveltekit()],
   test: {
     include: ["src/**/*.{test,spec}.{js,ts}"],

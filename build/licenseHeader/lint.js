e85d5273e3ee3d94cb753317147ac11c18fdf65e
Adam <team@readwise.io>
2024-07-27 13:53:58 +0100

    Add header to all files

--- build/licenseHeader/lint.js
+++ build/licenseHeader/lint.js
@@ -7,15 +7,6 @@
  * Copyright © 2024 Adam Lynch (https://adamlynch.com)
  */
 
-/*
- * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
- * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
- *
- * Project: Back Of Your Hand (https://backofyourhand.com)
- * Repository: https://github.com/adam-lynch/back-of-your-hand
- * Copyright © 2024 Adam Lynch (https://adamlynch.com)
- */
-
 import fs from "fs";
 import { globSync } from "glob";
 import createIgnoreInstance from "ignore";
@@ -101,7 +92,7 @@ for (const filePath of filePathsToCheck) {
   const extension = path.extname(filePath).replace(/^./, "");
   const licenseHeader = makeLicenseHeaderComment(extension);
 
-  if (content.startsWith(licenseHeader.split("\n")[0])) {
+  if (content.startsWith(licenseHeader.split("\n")[0].trim())) {
     continue;
   }
 

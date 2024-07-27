22f72dfd2916b8829592df574f37d9699656da80
Adam <team@readwise.io>
2024-07-27 13:41:14 +0100

    Add header to all files

--- build/lint-staged.config.js
+++ build/lint-staged.config.js
@@ -1,3 +1,12 @@
+/* 
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * 
+ * Project: Back Of Your Hand (https://backofyourhand.com)
+ * Repository: https://github.com/adam-lynch/back-of-your-hand
+ * Copyright © 2024 Adam Lynch (https://adamlynch.com)
+ */
+
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
  * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
@@ -35,12 +44,13 @@ export const fileExtensionsWhichSupportComments = [
   "yaml",
 ];
 
-const formatAndListTasks = ["prettier --write", "eslint --fix"];
+const eslint = "eslint --fix";
+const licenseHeader = "node ./build/licenseHeader/lint.js --write";
+const prettier = "eslint --fix";
 
 export default {
-  [`*.{${fileExtensionsWhichSupportComments.join(",")}}`]: [
-    "node ./build/licenseHeader/lint.js --write",
-    ...formatAndListTasks,
-  ],
-  "*.{json,md}": formatAndListTasks,
+  [`*.{${fileExtensionsWhichSupportComments.filter((extension) => !["sh", "yaml"].includes(extension)).join(",")}}`]:
+    [licenseHeader, prettier, eslint],
+  "*.{json,md}": [prettier],
+  [`*{.sh,*.yaml}`]: [licenseHeader],
 };

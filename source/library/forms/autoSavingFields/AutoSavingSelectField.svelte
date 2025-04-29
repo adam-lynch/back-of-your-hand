e87f48aff749a55d3999bd7ea6e6fd449b4cfb37
Adam <team@readwise.io>
2025-04-29 19:37:53 +0100

    Reapply "Add Organization plan modal"
    This reverts commit 171f2f8e52457ee6a85097f5bce428d78fdd9185.

--- source/library/forms/autoSavingFields/AutoSavingSelectField.svelte
+++ source/library/forms/autoSavingFields/AutoSavingSelectField.svelte
@@ -47,7 +47,6 @@
   const formSchema = yup.object({
     [name]: schema.label(fieldProps.labelText),
   });
-
   type Schema = yup.InferType<typeof formSchema>;
 
   const form = createForm<Schema>({
